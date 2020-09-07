bb9a07c76c5fc870aae7c4d03dabf1363ad5a1ca
Darshan Unadkat <darshanunadkat67@gmail.com>
2020-09-07 15:24:44 +0530

    Test

---
+++ src/resumeData.js
@@ -0,0 +1,109 @@
+let resumeData = {
+  "imagebaseurl":"https://darshan6799.github.io/",
+  "name": "Darshan Unadkat",
+  "role": "Full Stack Developer",
+  "linkedinId":"darshan",
+  "skypeid": "Your skypeid",
+  "roleDescription": "I like dabbling in software development and like to learn about new technologies, write technical articles or simply read books in my free time.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/darshan-unadkat",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/darshan6799",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"https://join.skype.com/invite/oY1TghUl1rcR",
+        "className":"fa fa-skype"
+      },
+      {
+        "name":"instagram",
+        "url":"http://instagram.com/darshan_here",
+        "className":"fa fa-instagram"
+      },
+    ],
+  "aboutme":"I have finished my B.E. in Computer Science and have started working in industry.",
+  "address":"India",
+  "education":[
+    {
+      "UniversityName":"Government Engineering College - Rajkot",
+      "specialization":"Compueter Engineering",
+      "MonthOfPassing":"2016 - ",
+      "YearOfPassing":"2020",
+    },
+    {
+      "UniversityName":"Shantvan School - Rajkot",
+      "specialization":"12th Science",
+      "MonthOfPassing":"2014 - ",
+      "YearOfPassing":"2016",
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Tatvasoft",
+      "specialization":"Software Engineer Trainee",
+      "MonthOfLeaving":"Jan/2020 - ",
+      "YearOfLeaving":"Current",
+    }
+   
+  ],
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"Asp .Net"
+    },
+    {
+      "skillname":"Python"
+    },
+    {
+      "skillname":"SQL"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
